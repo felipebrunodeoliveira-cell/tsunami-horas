08f9ec55ba32811c7995a210769fbb90b51c47be
felipebrunodeoliveira-cell <felipebrunodeoliveira@gmail.com>
2026-04-20 01:44:00 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='tsn-v4';
+const C='tsn-v5';
 const A=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))));
